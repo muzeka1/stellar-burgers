3f6ac34e5535090f51432c5d9820d6b691927c0e
Abdulla Khalitov <abdullahalitoff@gmail.com>
2025-02-23 19:04:34 +0300

    solved mistakes

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -14,24 +14,26 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName, location }) => (
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
         <>
-        <Link to='/' className={location.pathname == "/" ? styles.link_active : styles.link} state={{from: location}}>
-          <BurgerIcon type={location.pathname == '/' ? 'primary' : 'disabled'} />
-          <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </Link>
+          <Link to='/' className={location.pathname == "/" ? styles.link_active : styles.link} state={{ from: location }}>
+            <BurgerIcon type={location.pathname == '/' ? 'primary' : 'disabled'} />
+            <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
+          </Link>
         </>
         <>
-        <Link to='/feed' className={location.pathname.startsWith('/feed') ? styles.link_active : styles.link} state={{from: location}}>
-          <ListIcon type={location.pathname.startsWith('/feed') ? 'primary' : 'disabled'} />
-          <p className='text text_type_main-default ml-2'>Лента заказов</p>
+          <Link to='/feed' className={location.pathname.startsWith('/feed') ? styles.link_active : styles.link} state={{ from: location }}>
+            <ListIcon type={location.pathname.startsWith('/feed') ? 'primary' : 'disabled'} />
+            <p className='text text_type_main-default ml-2'>Лента заказов</p>
           </Link>
         </>
       </div>
       <div className={styles.logo}>
-        <Logo className='' />
+        <Link to='/' state={{ from: location }}>
+          <Logo className='' />
+        </Link>
       </div>
 
       <div className={styles.link_position_last}>
-        <Link to='/profile' className={location.pathname == ('/profile') ? styles.link_active : styles.link} state={{from: location}}>
+        <Link to='/profile' className={location.pathname == ('/profile') ? styles.link_active : styles.link} state={{ from: location }}>
           <ProfileIcon type={location.pathname == '/profile' ? 'primary' : 'disabled'} />
           <p className='text text_type_main-default ml-2'>
             {userName || 'Личный кабинет'}
